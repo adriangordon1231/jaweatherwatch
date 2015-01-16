fa263038725c5d32df9753a7e74a250dce5648d6
Adrian Gordon <adriangordon@outlook.com>
2015-01-15 19:19:53 -0800

    added the main application regions and stared the app in index.html

--- js/JAWeatherWatch.js
+++ js/JAWeatherWatch.js
@@ -1,8 +1,20 @@
+/*global Backbone */
+'use strict';
+
 window.JAWeatherWatch = new Backbone.Marionette.Application();
 
+
+JAWeatherWatch.addRegions({
+    menu:'#main-menu',
+    content:'#page-wrapper'
+});
 
 /*  Begins the Backbone history api on Applicaiton Start
 ________________________________________________________*/
 JAWeatherWatch.on('start',function(){
+    //tmp
+    console.log(this.content);
+    
     Backbone.history.start();
 });
+
