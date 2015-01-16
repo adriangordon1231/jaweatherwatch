41c30686f692d749bec9a415e0174e7822d68cdc
Adrian Gordon <adriangordon@outlook.com>
2015-01-16 12:57:03 -0800

    added a cities model that grabs the current city weather data from the open weather api

--- js/JAWeatherWatch.Cities.js
+++ js/JAWeatherWatch.Cities.js
@@ -2,9 +2,13 @@
 
 /*  Parishes Module
  *  Contians the models and collections that are intended to model the weather patterns in each parish     
-*/
+______________________________________________________________________________________________________*/
 JAWeatherWatch.module('Cities', function(Cities, JAWeatherWatch, Backbone, Marionette){
     
+    /*  Cities
+    *   This model is used to model the datat retued by the open weather api 
+    *   for each individual city in jamaica
+    ________________________________________________________________________*/
     Cities.City = Backbone.Model.extend({
         defaults:{
             // stores my api key as well the required query string to add to the request
@@ -12,16 +16,45 @@ JAWeatherWatch.module('Cities', function(Cities, JAWeatherWatch, Backbone, Mario
             country:'jamaica',
             parishName: false
         }, 
+        url:'http://api.openweathermap.org/data/2.5/weather?q=',
         initialize: function(){
             
-            // get json data from api
+            this.url = this.setQuery();
+            
+            this.fetch();
+            
+        },
+        /*  Function
+        *   creates the quersy string that will be sent off to the server
+        _________________________________________________________________*/
+        setQuery : function(){
+            
+            var query = this.url;
+            var search = query.concat(',',this.get('country'),this.get('apiKey'));
+            
+            return search;
         }
+        
     });
+    
+   
     
     
 });
 
 
+/*  City Model Test => Success
+_________________________________*/
+/*
 var test = new JAWeatherWatch.Cities.City({parishName:'kingston'});
 
-console.log(test.attributes);
+test.fetch({
+    success: function(){
+        console.log(test.attributes);
+    },
+    error: function(){
+        alert('error when fetching data');
+    }
+});
+*/
+
