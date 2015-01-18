a586ae0202f55da5ceb50f451b1dbb060e71b838
Adrian Gordon <adriangordon@outlook.com>
2015-01-18 01:21:53 -0800

    added an empty app routher and initlized it and the controller at the bottom of the index.html page

--- dist/js/JAWeatherWatch.Controller.js
+++ dist/js/JAWeatherWatch.Controller.js
@@ -1,8 +1,28 @@
 "use strict"
 
-JAWeatherWatch.module('App', function(Controller,JAWeatherWatch, Backbone, Marionette){
+JAWeatherWatch.module('App', function(App,JAWeatherWatch, Backbone, Marionette){
     
+    /*      Controller
+    *       This controller is used to orchestrate the general flow of the appliction
+    *       Bacially, it is used to pull everything together
+    */
     App.Controller = Marionette.Controller.extend({
         
+        start:function(){
+            
+            this.showMenu();
+        },
+        showMenu:function(){
+            JAWeatherWatch.menu.show(new JAWeatherWatch.Views.SideMenu());
+        }
+        
     });
+    
+    /*  Router
+    *   This object is used to controll routing within the application (I am not touching this for now lol)
+    */
+    App.Router = Marionette.AppRouter.extend({});
+    
+    
+    
 });
