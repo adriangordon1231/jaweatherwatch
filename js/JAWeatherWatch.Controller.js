31f334280453e3476ebc44ca7d49bf35a27c84e9
Adrian Gordon <adriangordon@outlook.com>
2015-01-27 12:20:59 -0800

    added comments to the app module

--- js/JAWeatherWatch.Controller.js
+++ js/JAWeatherWatch.Controller.js
@@ -11,15 +11,25 @@ JAWeatherWatch.module('App', function(App,JAWeatherWatch, Backbone, Marionette){
         dashboard: new JAWeatherWatch.Views.Dashboard(),
         parish: new JAWeatherWatch.Cities.Parish(),
         jamaica: new JAWeatherWatch.Cities.City({parishName:""}),
+        
+        /*  Start Function
+        *
+        *   Renders all of the appropriate views and sets ups the
+        *   necissary event listeners
+        */
         start:function(){
             
+            // Renders the side menu and the dashboard
             this.showMenu();
             this.showDashboard();
             
+            // listens to a change in the "Parish" model and rerenders the notifications and forecast sections
             this.listenTo(this.parish, 'change', function(){
                 this.dashboard.getRegion('notifications').show(this.notificationsInit());
                 this.dashboard.getRegion('forecast').show(this.forecastInit());
             });
+            
+            // Litstens to a change in the "jamaica" model and rerenders the 'island wide averages' section
             this.listenTo(this.jamaica, 'change', function(){
                 this.dashboard.getRegion('averages').show(this.averagesInit());
             });
@@ -42,9 +52,15 @@ JAWeatherWatch.module('App', function(App,JAWeatherWatch, Backbone, Marionette){
             this.dashboard.getRegion('averages').show(this.averagesInit());
             
         },
-       // initilizes the notifications view and returns a notification Marionette.ItemView Object
+      /*   NotificationsInit Function 
+       *
+       *    Initilizes returns a notification Marionette.ItemView Object
+       *    this function also fethces data from the openweater api and passes it (as a model) to 
+       *    the 'Notifications' ItemView
+       */
         notificationsInit: function(){
             
+            // gets the currently selected parish from the 'Parish' Model
             var city = new JAWeatherWatch.Cities.City({parishName:this.parish.get('name')});
             var notifications = new JAWeatherWatch.Views.Notifications({model:city});
             city.fetch({
@@ -60,6 +76,13 @@ JAWeatherWatch.module('App', function(App,JAWeatherWatch, Backbone, Marionette){
             return notifications;
             
         },
+        
+        /*  AveragesInit Function 
+       *
+       *    Initilizes and returns a notification Marionette.ItemView Object
+       *    this function also fethces data from the openweater api and passes it (as a model) to 
+       *    the 'Averages' ItemView
+       */
         averagesInit: function(){
             
             //initialize a new Cities Module with no parish name            
@@ -76,16 +99,22 @@ JAWeatherWatch.module('App', function(App,JAWeatherWatch, Backbone, Marionette){
             
             return  averages;
         }, 
+        
+      /*  AveragesInit Function 
+       *
+       *    Initilizes and returns a notification Marionette.ItemView Object
+       *    this function also fethces data from the openweater api and passes it (as a model) to 
+       *    the 'Averages' ItemView
+       */
         forecastInit: function(){
             
-            var estimates = new JAWeatherWatch.Cities.Forecast({parishName:this.parish.get('name')});
-            
+            // gets the currently selected parish from the Parish Model
+            var estimates = new JAWeatherWatch.Cities.Forecast({parishName:this.parish.get('name')});            
             var forecast = new JAWeatherWatch.Views.Forecast({model:estimates});
             
             estimates.fetch({
                 success: function(){
-                    console.log('forecast data fetched successfully');
-                    
+                    console.log('forecast data fetched successfully');                    
                 },
                 error: function(){
                     alert('could not fetch forecast data');
@@ -97,6 +126,8 @@ JAWeatherWatch.module('App', function(App,JAWeatherWatch, Backbone, Marionette){
         // Dynamically sets the parish name based on the entered url
        changeCity: function(query){
            this.parish.set('name',query);
+           
+           //todo: change the parish selector display to match the currently selected parish in the url
        }
         
     });
@@ -108,8 +139,6 @@ JAWeatherWatch.module('App', function(App,JAWeatherWatch, Backbone, Marionette){
         appRoutes:{
             ':query':'changeCity'
         }
-    });
-    
-    
+    });   
     
 });
